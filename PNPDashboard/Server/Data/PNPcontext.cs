5d697da0e4dc67628624333fb7349678d48d7c08
ASHISH-Dotco <83586671+ASHISH-Dotco@users.noreply.github.com>
2021-05-21 20:33:53 +0530

    Context Error Changes

--- PNPDashboard/Server/Data/PNPcontext.cs
+++ PNPDashboard/Server/Data/PNPcontext.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
-using PNPDashboard.Server.Models;
+using PNPDashboard.Shared.Models;
 
 #nullable disable
 
