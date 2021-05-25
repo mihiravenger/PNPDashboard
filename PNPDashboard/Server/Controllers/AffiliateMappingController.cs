eb1a0af29469438ce384ee3d97412a75ae4bd998
ASHISH-Dotco <83586671+ASHISH-Dotco@users.noreply.github.com>
2021-05-25 10:18:48 +0530

    Nav bar changes, Trying to insert Data in Tabel Format

---
+++ PNPDashboard/Server/Controllers/AffiliateMappingController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PNPDashboard.Server.Storage;
+using PNPDashboard.Shared;
+using PNPDashboard.Shared.Models;
+
+namespace PNPDashboard.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AffiliateMappingController : ControllerBase
+    {
+        private readonly IRepository<AffiliateMapping> _earningRepository;
+        public AffiliateMappingController(IRepository<AffiliateMapping> earningRepository)
+        {
+            _earningRepository = earningRepository;
+        }
+
+        [HttpGet]
+        public IEnumerable<AffiliateMapping> Get()
+        {
+            return _earningRepository.GetAll()
+                .OrderBy(AffiliateMapping => AffiliateMapping.CreatedDate);
+        }
+    }
+}
